1ecc35cd57f610276e2dafdaa7f939b61d61dc57
rborges98 <rcborges98@gmail.com>
2024-09-17 00:36:18 -0300

    refactor: add undefined verification

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -24,7 +24,7 @@ export default function Page() {
         content: sendEmail?.message
       })
 
-      if (!sendEmail.error) {
+      if (!sendEmail?.error) {
         const encryptedEmail = await encrypt(email)
 
         const verifyUrl = '/verify/' + encryptedEmail
